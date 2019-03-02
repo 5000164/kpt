3b90fbfb608e89deb9f7d3f5a287c3c7ca166291
Hiroshi Sugawara <5000164@users.noreply.github.com>
2019-03-03 00:59:51 +1100

    feat: 複数のユーザーで同じ値を画面に表示できるようにする

--- server/src/main/scala/WebServer.scala
+++ server/src/main/scala/WebServer.scala
@@ -1,11 +1,13 @@
-import akka.NotUsed
-import akka.actor.ActorSystem
+import java.util.UUID
+
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.ws.{Message, TextMessage}
 import akka.http.scaladsl.server.Directives._
-import akka.stream.ActorMaterializer
-import akka.stream.scaladsl.Flow
+import akka.stream.scaladsl.{Flow, GraphDSL, Merge, Sink, Source}
+import akka.stream.{ActorMaterializer, FlowShape, OverflowStrategy}
 
+import scala.collection.mutable
 import scala.concurrent.ExecutionContextExecutor
 import scala.io.StdIn
 
@@ -15,8 +17,48 @@ object WebServer {
     implicit val materializer: ActorMaterializer = ActorMaterializer()
     implicit val executionContext: ExecutionContextExecutor = system.dispatcher
 
-    def flow: Flow[Message, TextMessage.Strict, NotUsed] = Flow[Message].map {
-      case TextMessage.Strict(_) => TextMessage(scala.util.Random.alphanumeric.take(10).mkString)
+    case class Subscribe(id: String, actorRef: ActorRef)
+    case class UnSubscribe(id: String)
+
+    class BroadcastActor extends Actor {
+      val subscribers = mutable.HashMap.empty[String, ActorRef]
+      var count = 0
+
+      def receive: Receive = {
+        case Subscribe(id, actorRef) =>
+          subscribers += ((id, actorRef))
+          subscribers.values.foreach(_ ! count.toString)
+        case UnSubscribe(id) => subscribers -= id
+        case _ =>
+          count = count + 1
+          subscribers.values.foreach(_ ! count.toString)
+      }
+    }
+    val broadcastActor = system.actorOf(Props[BroadcastActor])
+
+    def flow: Flow[Message, TextMessage.Strict, ActorRef] = {
+      Flow.fromGraph(GraphDSL.create(Source.actorRef[String](bufferSize = 3, OverflowStrategy.fail)) { implicit builder => subscribeActor =>
+        import GraphDSL.Implicits._
+
+        val websocketSource = builder.add(Flow[Message].map {
+          case TextMessage.Strict(message) => message.toString
+        })
+        val uuid = UUID.randomUUID().toString
+        val connActorSource = builder.materializedValue.map[Any](Subscribe(uuid, _))
+        val merge = builder.add(Merge[Any](2))
+        val broadcastActorSink = Sink.actorRef(broadcastActor, UnSubscribe(uuid))
+
+        websocketSource ~> merge ~> broadcastActorSink
+        connActorSource ~> merge
+
+        val output = builder.add(Flow[String].map { message: String =>
+          TextMessage(message)
+        })
+
+        subscribeActor ~> output
+
+        FlowShape(websocketSource.in, output.out)
+      })
     }
 
     val route =
