748c2ee1d59e2702bc5b9257cc595fb62953c2b0
Hiroshi Sugawara <5000164@users.noreply.github.com>
2019-03-18 01:09:01 +1100

    feat: サーバーからのレスポンスもバイナリ形式にする

--- server/src/main/scala/WebServer.scala
+++ server/src/main/scala/WebServer.scala
@@ -2,10 +2,11 @@ import java.util.UUID
 
 import akka.actor.{Actor, ActorRef, ActorSystem, Props}
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.ws.{BinaryMessage, Message, TextMessage}
+import akka.http.scaladsl.model.ws.{BinaryMessage, Message}
 import akka.http.scaladsl.server.Directives._
 import akka.stream.scaladsl.{Flow, GraphDSL, Merge, Sink, Source}
 import akka.stream.{ActorMaterializer, FlowShape, OverflowStrategy}
+import akka.util.ByteString
 import myproto.item.Item
 
 import scala.collection.mutable
@@ -27,17 +28,17 @@ object WebServer {
       def receive: Receive = {
         case Subscribe(id, actorRef) => subscribers += ((id, actorRef))
         case UnSubscribe(id) => subscribers -= id
-        case content => subscribers.values.foreach(_ ! content)
+        case item: Item => subscribers.values.foreach(_ ! item)
       }
     }
     val broadcastActor = system.actorOf(Props[BroadcastActor])
 
     def flow: Flow[Message, Message, Any] = {
-      Flow.fromGraph(GraphDSL.create(Source.actorRef[String](bufferSize = 3, OverflowStrategy.fail)) { implicit builder => subscribeActor =>
+      Flow.fromGraph(GraphDSL.create(Source.actorRef[Item](bufferSize = 3, OverflowStrategy.fail)) { implicit builder => subscribeActor =>
         import GraphDSL.Implicits._
 
         val websocketSource = builder.add(Flow[Message].map {
-          case BinaryMessage.Strict(message) => Item.parseFrom(message.toArray).content
+          case BinaryMessage.Strict(message) => Item.parseFrom(message.toArray)
         })
         val uuid = UUID.randomUUID().toString
         val connActorSource = builder.materializedValue.map[Any](Subscribe(uuid, _))
@@ -47,9 +48,7 @@ object WebServer {
         websocketSource ~> merge ~> broadcastActorSink
         connActorSource ~> merge
 
-        val output = builder.add(Flow[String].map { message: String =>
-          TextMessage(message)
-        })
+        val output = builder.add(Flow[Item].map(item => BinaryMessage(ByteString(item.toByteArray))))
 
         subscribeActor ~> output
 
