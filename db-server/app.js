52243c56dbab4ccfa779bd176d87d1bf84014131
Hiroshi Sugawara <5000164@users.noreply.github.com>
2019-05-23 22:39:37 +1000

    feat: to save data

--- db-server/app.js
+++ db-server/app.js
@@ -1,16 +1,33 @@
 const express = require("express")
 const bodyParser = require("body-parser")
+const mongoose = require("mongoose")
 const app = express()
 const port = 8081
 
+const kptSchema = new mongoose.Schema({
+  keep: [String],
+  problem: [String],
+  try: [String],
+})
+const Kpt = mongoose.model("Kpt", kptSchema)
+
 app.use(bodyParser.json())
 
-app.post("/create", (req, res) => {
+app.post("/create", async (req, res) => {
+  await mongoose.connect("mongodb://127.0.0.1:27017/kpt", { useNewUrlParser: true })
+
+  const kpt = new Kpt(req.body)
+  await kpt.save()
+
   res.send()
 })
 
-app.post("/getList", (req, res) => {
-  res.send()
+app.post("/getList", async (req, res) => {
+  await mongoose.connect("mongodb://127.0.0.1:27017/kpt", { useNewUrlParser: true })
+
+  const result = await Kpt.find({}).exec()
+
+  res.send(result)
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
