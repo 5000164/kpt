0abe8549215d3a072ca00d8f1b6b4e6acbc4e81d
Hiroshi Sugawara <5000164@users.noreply.github.com>
2019-06-08 00:03:14 +1000

    feat: adding tests about a create feature

--- db-server/test/e2e/app.js
+++ db-server/test/e2e/app.js
@@ -15,6 +15,10 @@ describe("KPT", () => {
     })
 
     after(async () => {
+      await mongoose.disconnect()
+    })
+
+    afterEach(async () => {
       await Kpt.deleteMany({})
     })
 
@@ -42,6 +46,74 @@ describe("KPT", () => {
             keep: ["keep"],
             problem: ["problem"],
             try: ["try"],
+          },
+        ]
+      )
+    })
+
+    it("multiple values", async () => {
+      assert.deepStrictEqual(await Kpt.find({}), [])
+
+      await axios.post("http://127.0.0.1:8081/create", {
+        keep: ["keep1"],
+        problem: ["problem1", "problem2"],
+        try: ["try1", "try2", "try3"],
+      })
+
+      assert.deepStrictEqual(
+        (await Kpt.find({})).map(d =>
+          d.toJSON({
+            transform: (doc, ret, options) => {
+              delete ret._id
+              return ret
+            },
+            versionKey: false,
+          })
+        ),
+        [
+          {
+            keep: ["keep1"],
+            problem: ["problem1", "problem2"],
+            try: ["try1", "try2", "try3"],
+          },
+        ]
+      )
+    })
+
+    it("multiple times", async () => {
+      assert.deepStrictEqual(await Kpt.find({}), [])
+
+      await axios.post("http://127.0.0.1:8081/create", {
+        keep: ["keep1"],
+        problem: ["problem1"],
+        try: ["try1"],
+      })
+      await axios.post("http://127.0.0.1:8081/create", {
+        keep: ["keep2"],
+        problem: ["problem2"],
+        try: ["try2"],
+      })
+
+      assert.deepStrictEqual(
+        (await Kpt.find({})).map(d =>
+          d.toJSON({
+            transform: (doc, ret, options) => {
+              delete ret._id
+              return ret
+            },
+            versionKey: false,
+          })
+        ),
+        [
+          {
+            keep: ["keep1"],
+            problem: ["problem1"],
+            try: ["try1"],
+          },
+          {
+            keep: ["keep2"],
+            problem: ["problem2"],
+            try: ["try2"],
           },
         ]
       )
